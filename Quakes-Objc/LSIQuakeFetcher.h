3427ce77add30a9b86557264feedf25ce5eebc80
Paul Solt <PaulSolt@gmail.com>
2020-06-11 13:19:12 -0400

    Added new fetcher for quakes

---
+++ Quakes-Objc/LSIQuakeFetcher.h
@@ -0,0 +1,17 @@
+//
+//  LSIQuakeFetcher.h
+//  Quakes-Objc
+//
+//  Created by Paul Solt on 6/11/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIQuakeFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
